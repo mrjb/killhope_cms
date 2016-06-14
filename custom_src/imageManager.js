9496f0b63fbeb25324201e28f1011fee8c2e93f9
mrjb <jonathan.s.berry@googlemail.com>
2016-06-14 11:59:35 +0100

    Fixed json transfer issue.

--- custom_src/imageManager.js
+++ custom_src/imageManager.js
@@ -6,7 +6,7 @@ $(function(){
 
 	// Start by fetching the file data from scan.php with an AJAX request
 
-	$.get('imageHandler.php', function(data) {
+	$.getJSON('imageHandler.php', function(data) {
 
 		var response = [data],
 			currentPath = '',
